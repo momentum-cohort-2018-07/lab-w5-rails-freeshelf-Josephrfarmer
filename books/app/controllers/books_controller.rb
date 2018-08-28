b1e0a878b093cf95aaa75b28e4d6adc172c220d6
josephrfarmer <josephr.farmer24@gmail.com>
2018-08-28 16:13:53 -0400

    Day one finished Crud done.
    Next step is day 2

--- books/app/controllers/books_controller.rb
+++ books/app/controllers/books_controller.rb
@@ -9,24 +9,44 @@ class BooksController < ApplicationController
   #make a new album 
   def create 
     @book = Book.create(title: params[:title],
-                          author: params[:author],
-                          description: params[:description],
-                          URL: params[:URL]) 
+                        author: params[:author],
+                        description: params[:description],
+                        URL: params[:URL]) 
+    redirect_to books_path
   end 
 
   def show 
-    @book = Book.find(album_params[:id])
+    @book = Book.find(params[:id])
   end  
+
+  def edit
+    @book = Book.find(params[:id])
+  end
   
-  def edit 
+  def update 
+    @book = Book.find(params[:id])
+
+    respond_to do |format|
+      if @book.update(book_params)
+        format.html { redirect_to @book, notice: 'Book was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+
   end  
 
   def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    respond_to do |format|
+      format.html { redirect_to @book, notice: 'Book was successfully destroyed.' }
+    end
   end 
 
   private
-    def album_params
-      params.permit(:id, :title, :artist, :year)
+    def book_params
+      params.require(:book).permit(:title, :author, :description, :URL)
     end
   
 
